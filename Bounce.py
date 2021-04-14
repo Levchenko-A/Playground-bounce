0f70e559accd6754544a4355863f6b43cf995a67
Andrii Levchenko <82201855+Levchenko-A@users.noreply.github.com>
2021-04-14 10:29:31 +0300

    "added score and game over message"

--- Bounce.py
+++ Bounce.py
@@ -1,11 +1,14 @@
 from tkinter import *
 import time
 import random
+import os
+import sys
 
 
 # ===> Defining the ball and it's behaviour
 class Ball:
-    def __init__(self, canvas, paddle, color):
+    def __init__(self, canvas, paddle, color, score):
+        self.score = score
         self.canvas = canvas
         self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)  # creating a ball of given color
@@ -27,6 +30,8 @@ class Ball:
             self.hit_bottom = True  # loss condition
         if self.hit_paddle(pos) == True:
             self.y = -3
+            self.score += 1
+            canvas.itemconfig(score_text, text=int(self.score))  # score counter
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
@@ -67,21 +72,34 @@ class Paddle:
         self.x = 2
 
 
+def start_game(event):
+    if event.keysym == 'Return':
+        while True:
+            if ball.hit_bottom == False:
+                ball.draw()
+                paddle.draw()
+            if ball.hit_bottom == True:
+                time.sleep(0.5)
+                game_over = canvas.create_rectangle(150, 100, 350, 200, fill='black', outline='white')
+                game_over_text = canvas.create_text(250, 150, text='Game Over', font=('Arial', 20), fill='white')
+            tk.update_idletasks()
+            tk.update()
+            time.sleep(0.01)
+
 # ===> game window and it`s characteristics
 tk = Tk()
+score = 0
 tk.title('Bounce - Game')  # title of the window
 tk.resizable(0, 0)  # fixed size
 tk.wm_attributes('-topmost', 1)  # on top of all windows
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+score_rect = canvas.create_polygon(400, 10, 490, 10, 490, 60, 400, 60, 405, 55, 485, 55, 485, 15, 405, 15, 405, 55, 400,
+                                   60, fill='red', outline='black')
+score_text = canvas.create_text(445, 35, text=int(score), font=("Arial", 15))
 tk.update()
 paddle = Paddle(canvas, 'magenta')
-ball = Ball(canvas, paddle, 'blue')
-# ===> main loop
-while True:
-    if ball.hit_bottom == False:
-        ball.draw()
-        paddle.draw()
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.01)
+ball = Ball(canvas, paddle, 'blue', score)
+canvas.bind_all("<KeyPress-Return>", start_game)
+tk.mainloop()
+
